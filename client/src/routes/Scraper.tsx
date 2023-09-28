21b1c36a1ff674f0a3c1ee9ce60b6b0ee068be55
cdb61092 <36825464+cdb61092@users.noreply.github.com>
2023-09-27 20:06:49 -0500

    Create filters for remote type, date posted, experience level, and skill

--- client/src/routes/Scraper.tsx
+++ client/src/routes/Scraper.tsx
@@ -1,18 +1,17 @@
 import {useState} from "react";
-import {Filters} from "../types";
-import {Button, Input} from "@nextui-org/react";
+import {type Filters, RemoteType, ExperienceLevel, DeveloperSkill} from "../types";
+import { DatePosted } from "../types";
+import {Button, Input, Select, SelectItem} from "@nextui-org/react";
 
 export const Scraper = () =>  {
     const [filters, setFilters] = useState<Filters>({
         searchTerm: 'Software Engineer',
         where: 'Kansas City, MO',
-        remote: {
-            enabled: false,
-            selector: 'button#filter-remotejob',
-        },
+        remoteType: null,
+        datePosted: null,
+        experienceLevel: null,
+        developerSkill: null
     });
-
-    const setWhere = (where: string) => setFilters((filters) => ({...filters, where}))
 
     return (
         <div>
@@ -24,7 +23,55 @@ export const Scraper = () =>  {
 
                 {/* Location filter */}
                 <Input label='Location' type="text" placeholder="Where" value={filters.where}
-                       onChange={(e) => setWhere(e.target.value)}/>
+                       onChange={(e) => setFilters({...filters, where: e.target.value})}/>
+                <Select
+                    label="Date Posted"
+                    placeholder="All"
+                    className="max-w-xs"
+                    onChange={(e) => setFilters({...filters, datePosted: e.target.value as DatePosted})}
+                >
+                    {Object.values(DatePosted).map((value) => {
+                        return <SelectItem key={value}  value={value}>
+                            {value}
+                        </SelectItem>
+                    })}
+                </Select>
+                <Select
+                    label="Remote Type"
+                    placeholder="All"
+                    className="max-w-xs"
+                    onChange={(e) => setFilters({...filters, remoteType: e.target.value as RemoteType})}
+                >
+                    {Object.values(RemoteType).map((value) => {
+                        return <SelectItem key={value}  value={value}>
+                            {value}
+                        </SelectItem>
+                    })}
+                </Select>
+                <Select
+                    label="Experience Level"
+                    placeholder="All"
+                    className="max-w-xs"
+                    onChange={(e) => setFilters({...filters, experienceLevel: e.target.value as ExperienceLevel})}
+                >
+                    {Object.values(ExperienceLevel).map((value) => {
+                        return <SelectItem key={value}  value={value}>
+                            {value}
+                        </SelectItem>
+                    })}
+                </Select>
+                <Select
+                    label="Developer Skill"
+                    placeholder="All"
+                    className="max-w-xs"
+                    onChange={(e) => setFilters({...filters, developerSkill: e.target.value as DeveloperSkill})}
+                >
+                    {Object.values(DeveloperSkill).map((value) => {
+                        return <SelectItem key={value}  value={value}>
+                            {value}
+                        </SelectItem>
+                    })}
+                </Select>
             </div>
             <Button onClick={() => initiateScraping(filters)}>
                 Scrape Indeed
@@ -42,7 +89,10 @@ const initiateScraping = (filters: Filters) => {
         "body": JSON.stringify({
             searchTerm: filters.searchTerm,
             where: filters.where,
-            remote: filters.remote
+            remoteType: filters.remoteType,
+            datePosted:  filters.datePosted,
+            experienceLevel: filters.experienceLevel,
+            developerSkill: filters.developerSkill
         })
     }).then(() => console.info('Scraping started'));
 }
--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -7,11 +7,50 @@ export interface Job {
     location: string;
 }
 
+export enum DatePosted {
+    Last24Hours = 'Last 24 hours',
+    Last3Days = 'Last 3 days',
+    Last7Days = 'Last 7 days',
+    Last14Days = 'Last 14 days',
+}
+
+export enum RemoteType {
+    Remote = 'Remote',
+    Hybrid = 'Hybrid',
+}
+
+export enum ExperienceLevel {
+    None  = 'None',
+    Entry = 'Entry',
+    Mid   = 'Mid',
+    Senior = 'Senior',
+}
+
+export enum DeveloperSkill {
+    JavaScript = 'JavaScript',
+    TypeScript = 'TypeScript',
+    React = 'React',
+    Angular = 'Angular',
+    Vue = 'Vue',
+    Node = 'Node',
+    Python = 'Python',
+    Java = 'Java',
+    CSharp = 'C#',
+    CPlusPlus = 'C++',
+    C = 'C',
+    Go = 'Go',
+    Ruby = 'Ruby',
+    PHP = 'PHP',
+    Swift = 'Swift',
+    Kotlin = 'Kotlin',
+    Rust = 'Rust',
+}
+
 export interface Filters {
     searchTerm: string;
     where: string;
-    remote: {
-        enabled: boolean;
-        selector: string;
-    }
+    remoteType: RemoteType | null;
+    datePosted: DatePosted | null;
+    experienceLevel: ExperienceLevel | null;
+    developerSkill:  DeveloperSkill | null;
 }
